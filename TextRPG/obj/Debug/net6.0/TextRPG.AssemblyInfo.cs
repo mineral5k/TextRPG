c273b3e3f33ad63c77f446457b4f239cb75830c0
mineral5k <rkskek169@gmail.com>
2024-09-26 09:27:18 +0900

    gotignore
    깃 이그노어를 비주얼 스튜디오로 변경

--- TextRPG/obj/Debug/net6.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/obj/Debug/net6.0/TextRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e57f6f9e281c539c4b46c0f9ae1f80edca5fe27")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
